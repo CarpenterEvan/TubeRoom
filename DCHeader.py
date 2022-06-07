2ebf8849f844ff6740efbe9984bd96da41fd379f
Evan <evan.carpenter14@gmail.com>
2022-06-07 00:00:04 -0400

    Update DCHeader.py
    Added documentation and implemented a counter

--- DCHeader.py
+++ DCHeader.py
@@ -1,13 +1,37 @@
+'''
+The following text describes the code, use it as a reference, not a book!!
+    1)  Get the date and time for today, I use the time and date in the file, and the date for the file name
+            For example, CAENPS_20220512_test.log for 2022-05-12
+
+    2)  However, if CAENPS_20220512_test.log already exists and the date has not changed, then the name should be 
+            CAENPS_20220512_test2.log, so I check if a file of the same date already exists. 
+            Just as an extra precaution, I have the code print what the name and time will be just to double check.
+            The code then asks for the operator name. If the file name printed before it does not look good for some reason, 
+            put the operator name as "stop" and it will exit the program. 
+
+    3)  The code prompts for the tube IDs, after scanning all of them, it prints the IDs scanned for a double check.
+
+    4)  Final yes/no verification before writing to the file. 
+
+    5)  Writing to the file, I take the lines from the example file and replace the strings with the variables defined in the code
+            I write with "a" to append, to further minimize the possibility of overwriting data
+'''
+
 from os.path import exists
 from sys import exit
 import datetime
-
+'''
+1) ###########################################################################################
+'''
 year = datetime.date.today().strftime("%Y")
 month = datetime.date.today().strftime("%m")
 day = datetime.date.today().strftime("%d")
 time = datetime.datetime.now().strftime("%H:%M:%S")
 in_file_date = f"{year}-{month}-{day}" # I use separate variable here because I also need to name the file.
 
+'''
+2) ###########################################################################################
+'''
 file_exists = exists(f"./Google Drive/Shared drives/sMDT Tube Testing Reports/CAEN/CAENPS_{year}{month}{day}_test.log")
 if file_exists==False:
     processed_file_exists = exists(f"./Google Drive/Shared drives/sMDT Tube Testing Reports/CAEN/CAENPS_{year}{month}{day}_test.log")
@@ -20,15 +44,39 @@ print(time)
 operator = input("Operator: ")
 if operator == "stop":
     exit()
+'''
+3) ###########################################################################################
+'''
+
+counter = 0
 DC_tube_IDs = " "
 while True:
-    this_tube = input("Tube ID: ")
+    board_number = 4 if counter <= 23 else 1 
+    this_tube = input(f"Board {board_number} Position {counter % 24}: ")
+
     if this_tube != "stop":
         DC_tube_IDs += (this_tube) + " "  
     if this_tube == "stop":
         break
+    counter += 1
+
 print(DC_tube_IDs)
-print(len(DC_tube_IDs)//8)
+
+'''
+4) ###########################################################################################
+'''
+
+finish = input("Finish? [y/n]:")
+if finish == "y":
+    pass
+elif finish == "n":
+    exit()
+elif (finish != "y") and (finish != "n"):
+    exit() 
+
+'''
+4) ###########################################################################################
+'''
 
 with open("./Google Drive/Shared drives/sMDT Tube Testing Reports/CAEN/_CAENPS_2022MMDD_template.log", 'r') as Template:
     with open(file_name, 'a') as Output:
@@ -39,4 +87,4 @@ with open("./Google Drive/Shared drives/sMDT Tube Testing Reports/CAEN/_CAENPS_2
         Output.writelines(lines[3:13])
         Output.write(lines[14][0:15]+ DC_tube_IDs + "\n")
         Output.write(lines[15])
-
+print("All Done! :)")
